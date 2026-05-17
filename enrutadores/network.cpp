daf1dcee8d8c90b3eb9e5c1b4e55f694ceb4f329
Tav019 <galonso.valencia1@udea.edu.co>
2026-05-17 17:12:41 -0500

    Terminación clase network

--- enrutadores/network.cpp
+++ enrutadores/network.cpp
@@ -26,10 +26,10 @@ void Network::agregarRouter(string name){
 
         Router r;
 
-        routers.insert(pair<int, string>(numRouters, name)); //gets added to the list of routers
+        routers.insert(pair<int, string>(numRouters, name));
         numRouters++;
 
-        for(int i = 0; i<numRouters; i++){ //create links with all routers
+        for(int i = 0; i<numRouters; i++){
 
             if(name == routers[i])
                 r.agregarLink(routers[i], 0);
@@ -37,7 +37,7 @@ void Network::agregarRouter(string name){
                 r.agregarLink(routers[i]);
         }
 
-        tablaEnrutamiento.insert(pair<string, Router>(name, r)); //add new router to routing table
+        tablaEnrutamiento.insert(pair<string, Router>(name, r));
 
         for(tablaEnrutamiento_iterador=tablaEnrutamiento.begin(); tablaEnrutamiento_iterador != tablaEnrutamiento.end(); tablaEnrutamiento_iterador++) //adding a link with "name" to all existing routers
             tablaEnrutamiento_iterador->second.agregarLink(name);
@@ -46,12 +46,12 @@ void Network::agregarRouter(string name){
 
 void Network::eliminarRouter(string name){
 
-    tablaEnrutamiento.erase(name); //delete router from routing table
+    tablaEnrutamiento.erase(name);
 
     for(tablaEnrutamiento_iterador = tablaEnrutamiento.begin(); tablaEnrutamiento_iterador != tablaEnrutamiento.end(); tablaEnrutamiento_iterador++) //deleting all links with 'name' from all existing routers
         tablaEnrutamiento_iterador->second.eliminarLink(name);
 
-    if(tablaEnrutamiento.size()>1){ // Creates a new "numRouters" that doesn't include "name"
+    if(tablaEnrutamiento.size()>1){
         map<int, string>temp;
         int temp_index = 0;
 
@@ -195,4 +195,170 @@ void Network::dijkstra(string r1, string r2){
     mostrarSolucion(dist, parent, origin, destiny);
 
     eliminarGrafo(graph);
+}
+
+void Network::agregarLink(string r1, string r2, int cost){
+
+    tablaEnrutamiento[r1].agregarLink(r2, cost);
+    tablaEnrutamiento[r2].agregarLink(r1, cost);
+}
+
+void Network::eliminarLink(string r1, string r2){
+
+    tablaEnrutamiento[r1].eliminarLink(r2);
+    tablaEnrutamiento[r2].eliminarLink(r1);
+}
+
+void Network::modificarLink(string r1, string r2, int cost){
+
+    tablaEnrutamiento[r1].modificarLink(r2, cost);
+    tablaEnrutamiento[r2].modificarLink(r1, cost);
+}
+
+
+void Network::generarRed(){
+
+    short name_code, gen_routers = 2 + (rand() % (20 - 2 + 1));
+    string name;
+    int numLinks, r1, r2, c, prev_links = 0;
+
+    while(gen_routers > numRouters){
+        //numero aleatorio de routers
+        name_code = 65 + (rand() % (90 - 65 + 1));
+        name = char(name_code);
+
+        if(!existeRouter(name)){
+            agregarRouter(name);
+        }
+    }
+
+    for(int i = 0; i < numRouters; i++){
+        //Debe tener al menos un link
+        if(!tablaEnrutamiento[routers[i]].estaEnlazado()){
+
+            while(true){
+
+                name_code = rand() % numRouters;
+
+                if(name_code != i){
+
+                    c = 1 + (rand() % (100));
+
+                    agregarLink(routers[i], routers[name_code], c);
+                    prev_links++;
+
+                    break;
+                }
+            }
+        }
+    }
+
+    numLinks = (((numRouters-1) * (numRouters))/2) - prev_links;
+
+    if(numLinks > 0)
+        numLinks = 1 + (rand() % (numLinks));
+
+    while(numLinks > 0){
+        //numero aleatorio de links
+        r1 = rand() % (numRouters);
+        r2 = rand() % (numRouters);
+
+        if(r1 != r2){
+
+            if(existeLink(routers[r1], routers[r2]) == false){
+
+                c = 1 + (rand() % (100));
+
+                modificarLink(routers[r1], routers[r2], c);
+
+                numLinks--;
+            }
+        }
+    }
+}
+
+void Network::importarRed(string file_name){
+
+    fstream file (file_name,  fstream::in | fstream::binary);
+
+    if(file.is_open()){
+
+        string temp, r1, r2;
+        int cost;
+        short counter = 1;
+
+        while (file >> temp){
+
+            if(counter == 1){ //router 1
+                r1 = temp;
+                agregarRouter(r1);
+            }
+            else if (counter == 2){ //router 2
+                r2 = temp;
+                agregarRouter(r2);
+            }
+            else if (counter == 3){ //cost
+                cost = stoi(temp);
+
+                tablaEnrutamiento[r1].modificarLink(r2, cost);
+                tablaEnrutamiento[r2].modificarLink(r1, cost);
+
+                counter = 0;
+            }
+
+            counter++;
+        }
+        file.close();
+        cout << "Red importada.";
+    }
+    else {
+        cout<<"Error importando la red."<<endl;
+    }
+}
+
+void Network::guardarRed(string file_name){
+
+    fstream file (file_name,  fstream::out | fstream::binary);
+
+    if(file.is_open()){
+
+        string data;
+        string key1, key2;
+        int cost;
+
+        for(int i = 0; i < numRouters; i++){
+
+            key1 = routers[i];
+
+            for(int j = 0; j < numRouters; j++){
+
+                key2 = routers[j];
+
+                cost = tablaEnrutamiento[key1].links[key2];
+
+                if(cost>0){
+                    data += key1;
+                    data += ' ';
+                    data += key2;
+                    data += ' ';
+                    data += to_string(cost);
+                    data += "\n";
+                }
+            }
+        }
+
+        file.write(data.c_str(), data.length());
+
+        file.close();
+        cout<<"Red exportada correctamente"<<endl;
+    }
+    else {
+        cout<<"No se puedo abrir el archivo."<<endl;
+    }
+}
+
+void Network::vaciarRed(){
+    routers.clear();
+    tablaEnrutamiento.clear();
+    numRouters = 0;
 }
